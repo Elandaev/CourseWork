11b063593013e0c5d9a36ea7f77e36539b592e67
Elandaev <p.elandaev@gmail.com>
2021-12-28 09:16:07 +0300

    add algorithm>

--- include/SearchExtr.h
+++ include/SearchExtr.h
@@ -1,6 +1,7 @@
 #include "math.h"
 #include "iostream"
 #include <vector>
+#include <algorithm> 
 
 double SearchExtrFinally(const double x1, const double x2, double (*fcnPtr)(double),
                          double epsilon, double r);
